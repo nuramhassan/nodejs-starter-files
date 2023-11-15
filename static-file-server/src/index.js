0b9590ae0de8fb9658a731f1ef9f9cadda62f58e
Nura Hassan <nuramhassan2@gmail.com>
2023-11-15 20:13:09 +0000

    Create Server Task

--- static-file-server/src/index.js
+++ static-file-server/src/index.js
@@ -1,1 +1,11 @@
-/* Add your server code here*/
+const http = require("http");
+
+http.createServer(function (req, res) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json"); // Set Content-Type(tells client what type of content to process data ) to JSON
+  const responseData = {
+    greeting: 'Hey',
+    name: 'Replace with your name'
+  };
+  res.end(JSON.stringify(responseData)); // Convert the object to JSON string before sending
+}).listen(8080);
