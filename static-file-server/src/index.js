0a291b898284dc364a35dc4b1bb9f1d19e89f3cd
Nura Hassan <nuramhassan2@gmail.com>
2023-11-27 20:38:16 +0000

    static-task

--- static-file-server/src/index.js
+++ static-file-server/src/index.js
@@ -1,11 +1,24 @@
 const http = require("http");
+const express = require('express')
+const app = express()
+const path = require('path')
 
-http.createServer(function (req, res) {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json"); // Set Content-Type(tells client what type of content to process data ) to JSON
-  const responseData = {
-    greeting: 'Hey',
-    name: 'Replace with your name'
-  };
-  res.end(JSON.stringify(responseData)); // Convert the object to JSON string before sending
-}).listen(8080);
+app.use('/content',express.static(path.join(__dirname,'public')))
+
+
+app.get('/',(req, res) =>{
+  res.sendFile('./public/index.html', {root:__dirname})
+}
+)
+
+app.listen(8080);
+
+// http.createServer(function (req, res) {
+//   res.statusCode )= 200;
+//   res.setHeader("Content-Type", "application/json"); // Set Content-Type(tells client what type of content to process data ) to JSON
+//   const responseData = {
+//     greeting: 'Hey',
+//     name: 'Nura'
+//   };
+//   res.end(JSON.stringify(responseData)); // Convert the object to JSON string before sending
+// }).listen(8080);
